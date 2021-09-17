df86ef42369a8b6c197b406599d7beeacf1d72dd
Tsania Warda <tsaniawarda2@gmail.com>
2021-09-17 21:13:22 +0700

    Mencoba push Github

---
+++ pertemuan2/index.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+Tsania Warda Listianisari
+193040143
+https://github.com/tsaniawarda2/prakweb2021_oophp_193040143.git
+Pertemuan 2 - 17 September 2021
+*/
+?>
+
+<?php 
+// Standar Output
+// echo, print
+// print_r
+// var_dump
+
+echo "Tsania Warda Listianisari"
+
+?>
